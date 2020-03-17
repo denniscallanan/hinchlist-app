3da4d703263a1ca1e347311bdfd706a64800d233
Dennis Callanan <dennis@callanan.ie>
2020-03-17 20:06:38 +0000

    add: basic Facebook authentication

--- ios/HinchList/AppDelegate.m
+++ ios/HinchList/AppDelegate.m
@@ -13,10 +13,24 @@
 #import <UMCore/UMModuleRegistry.h>
 #import <UMReactNativeAdapter/UMNativeModulesProxy.h>
 #import <UMReactNativeAdapter/UMModuleRegistryAdapter.h>
-
+#import <FBSDKCoreKit/FBSDKCoreKit.h>
+    
 @implementation AppDelegate
 
 @synthesize window = _window;
+
+- (BOOL)application:(UIApplication *)application
+            openURL:(NSURL *)url
+            options:(NSDictionary<UIApplicationOpenURLOptionsKey,id> *)options {
+
+  BOOL handled = [[FBSDKApplicationDelegate sharedInstance] application:application
+    openURL:url
+    sourceApplication:options[UIApplicationOpenURLOptionsSourceApplicationKey]
+    annotation:options[UIApplicationOpenURLOptionsAnnotationKey]
+  ];
+  // Add any custom logic here.
+  return handled;
+}
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
